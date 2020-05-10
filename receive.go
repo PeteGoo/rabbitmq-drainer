837ec8da40db5d925b4807d545773ed83138e04b
Peter Goodman <pete@petegoo.com>
2020-05-11 11:37:30 +1200

    Use the connection string passed itn

--- receive.go
+++ receive.go
@@ -7,7 +7,7 @@ import (
 )
 
 func receive(connString string, queue *string, count *int) {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	conn, err := amqp.Dial(connString)
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
